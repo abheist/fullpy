3ff3b261274ecfdf07fceba2e566404f6e8dd620
Abhishek Kumar Singh <Abhishek Kumar Singh>
2016-04-17 14:42:27 +0530

    finished drawing circle through squares

--- mindstorm.py
+++ mindstorm.py
@@ -1,9 +1,11 @@
 import turtle
 
 def draw_square(some_turtle):
-	for x in range(4):
-		some_turtle.forward(100)
-		some_turtle.right(90)
+	for i in range(40):
+		for x in range(4):
+			some_turtle.forward(100)
+			some_turtle.right(90)
+		some_turtle.right(10)
 
 def draw_art():
 	window = turtle.Screen()
@@ -12,13 +14,13 @@ def draw_art():
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("yellow")
-	brad.speed(2)
+	brad.speed(0.5)
 	draw_square(brad)
-	#create the turtle Angie - Draws a circle
-	angie = turtle.Turtle()
-	angie.shape("arrow")
-	angie.color("blue")
-	angie.circle(100)
+	# create the turtle Angie - Draws a circle
+	# angie = turtle.Turtle()
+	# angie.shape("arrow")
+	# angie.color("blue")
+	# angie.circle(100)
 
 	window.exitonclick()
 
